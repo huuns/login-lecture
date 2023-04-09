5a9d082d9f226d56116847d9ace026f46892c4d5
jeehoon.park <bomee23@gmail.com>
2023-04-09 20:29:53 +0900

    commit login

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./routes/home");
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어 등록 메서드
+
+module.exports = app;
+
+package.json
